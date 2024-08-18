e1b3725a057ca816353ee43906bfb3045fe786b7
sergeik453 <sergei.kucheiko@gmail.com>
2024-08-18 11:58:22 -0400

    Initial Commit

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    # Read the text to analyse and initialise variables
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    myobj = { "raw_document": { "text": text_to_analyse } }
+
+    # Make a POST request to the API with the payload and headers
+    response = requests.post(url, json=myobj, headers=header)
+
+    # Parse the response from the API
+    formatted_response = json.loads(response.text)
+    
+    # Extract the emotion scores
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+    dominant_emotion = max(emotions, key=emotions.get)
+    
+    output = {
+        'anger': emotions['anger'],
+        'disgust': emotions['disgust'],
+        'fear': emotions['fear'],
+        'joy': emotions['joy'],
+        'sadness': emotions['sadness'],
+        'dominant_emotion': dominant_emotion
+    }
+    
+    return output
+
+
